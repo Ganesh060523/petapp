c227bcecc69e2409c4aefb2206cf5edc7593038c
Ganesh0605 <131640080+Ganesh0605@users.noreply.github.com>
2025-01-07 16:00:17 +0530

    commit for cloud api url2

--- src/app/service/configuration/environment.ts
+++ src/app/service/configuration/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
    // apiUrl: 'http://localhost:8080', // or the actual API URL
-      apiUrl: 'https://spring-pet-app-production.up.railway.app/', // or the actual API URL
+      apiUrl: 'https://spring-pet-app-production.up.railway.app/' // or the actual API URL
   };
