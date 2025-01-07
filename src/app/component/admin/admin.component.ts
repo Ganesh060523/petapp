f33dba6ffbd8ec747253d34a750a5c16c54c4a8c
Ganesh060523 <ganimaster0605@gmail.com>
2025-01-07 17:04:22 +0530

    commit 2 on jan 7

--- src/app/component/admin/admin.component.ts
+++ src/app/component/admin/admin.component.ts
@@ -13,11 +13,11 @@ import { Router, RouterLink } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { PetComponent } from "./pet/pet.component";
 import { RequestComponent } from "./request/request.component";
-import { GraphComponent } from "./graph/graph.component";
+
 
 @Component({
   selector: 'app-admin',
-  imports: [CommonModule, FormsModule, RouterLink, UserComponent, PetComponent, RequestComponent, GraphComponent],
+  imports: [CommonModule, FormsModule, RouterLink, UserComponent, PetComponent, RequestComponent],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'] // Corrected to use styleUrls (plural)
 })
@@ -26,9 +26,7 @@ export class AdminComponent implements OnInit {
   selectedPetId: number | null = null;
   selectedRequestId: number | null = null;
 
-  userGraphData: any;
-  petGraphData: any;
-  requestGraphData: any;
+ 
 
   constructor(
     private router: Router,
@@ -38,9 +36,7 @@ export class AdminComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loadUserGraph();
-    this.loadPetGraph();
-    this.loadRequestGraph();
+    
   }
 
   scrollToSection(sectionId: string): void {
@@ -50,66 +46,5 @@ export class AdminComponent implements OnInit {
     }
   }
 
-  loadUserGraph() {
-    this.userService.getAllUsers().subscribe((response) => {
-      const users = response.data;  // Assuming data is in response.data
-      const roles = users.reduce((acc: any, user) => {
-        acc[user.role] = (acc[user.role] || 0) + 1;
-        return acc;
-      }, {});
-
-      this.userGraphData = {
-        labels: Object.keys(roles),
-        datasets: [
-          {
-            label: 'Users by Role',
-            data: Object.values(roles),
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          },
-        ],
-      };
-    });
-  }
-
-  loadPetGraph() {
-    this.petService.getAllPets().subscribe((response) => {
-      const pets = response.data;  // Assuming data is in response.data
-      const types = pets.reduce((acc: any, pet) => {
-        acc[pet.type] = (acc[pet.type] || 0) + 1;
-        return acc;
-      }, {});
-
-      this.petGraphData = {
-        labels: Object.keys(types),
-        datasets: [
-          {
-            label: 'Pets by Type',
-            data: Object.values(types),
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
-          },
-        ],
-      };
-    });
-  }
-
-  loadRequestGraph() {
-    this.petRequestService.getAllAdoptionRequests().subscribe((response) => {
-      const requests = response.data;  // Assuming data is in response.data
-      const statuses = requests.reduce((acc: any, request) => {
-        acc[request.status] = (acc[request.status] || 0) + 1;
-        return acc;
-      }, {});
-
-      this.requestGraphData = {
-        labels: Object.keys(statuses),
-        datasets: [
-          {
-            label: 'Requests by Status',
-            data: Object.values(statuses),
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
-          },
-        ],
-      };
-    });
-  }
+  
 }
